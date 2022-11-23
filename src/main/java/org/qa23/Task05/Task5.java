27d9683ddb7d74992eb21484e5b8391fec4de04d
xcx1805 <dmytro.myroshnychenko.Extern@atruvia.de>
2022-11-23 17:59:10 +0200

    add template

--- src/main/java/org/qa23/Task05/Task5.java
+++ src/main/java/org/qa23/Task05/Task5.java
@@ -3,20 +3,23 @@ package org.qa23.Task05;
 import java.util.Scanner;
 
 /*
-* Пользователь вводит с клавиатуры число.
-* Необходи- мо проверить его на четность
-* и нечетность. Если число четное требуется
-* вывести на экран число и надпись «Even number».
-*  Если число нечетное выведите на экран число и надпись «Odd number».
+Задание 5
+Пользователь с клавиатуры вводит количество часов.
+Если полученное значение находится в диапазоне от 0 до 6
+нужно вывести надпись «Good Night», если в диапазоне
+от 6 до 13 «Good Morning», если в диапазоне от 13 до 17 «Good Day»,
+если в диапазоне от 17 до 0 «Good Evening».
+Верхняя граница диапазона не включается.
+Например, число 6 относится к 6 до 13.
 * */
 public class Task5 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-            int i = console.nextInt();
-            if (i%2 ==0) {
-                System.out.println("Event number");
-            } else {
-                System.out.println("Odd number");
-            }
+        int i = console.nextInt();
+        if (i % 2 == 0) {
+            System.out.println("Event number");
+        } else {
+            System.out.println("Odd number");
+        }
     }
 }
