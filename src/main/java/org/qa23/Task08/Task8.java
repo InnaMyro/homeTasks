acf6173c68613f6e7a7075a3709beaa6f3e9abcd
xcx1805 <dmytro.myroshnychenko.Extern@atruvia.de>
2022-11-23 17:48:53 +0200

    add template

---
+++ src/main/java/org/qa23/Task08/Task8.java
@@ -0,0 +1,22 @@
+package org.qa23.Task08;
+
+import java.util.Scanner;
+
+/*
+* Пользователь вводит с клавиатуры число.
+* Необходи- мо проверить его на четность
+* и нечетность. Если число четное требуется
+* вывести на экран число и надпись «Even number».
+*  Если число нечетное выведите на экран число и надпись «Odd number».
+* */
+public class Task8 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+            int i = console.nextInt();
+            if (i%2 ==0) {
+                System.out.println("Event number");
+            } else {
+                System.out.println("Odd number");
+            }
+    }
+}
