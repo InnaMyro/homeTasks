76997c7cd59f670561dcf989a713bbdf023966df
xcx1805 <dmytro.myroshnychenko.Extern@atruvia.de>
2022-12-10 18:57:17 +0100

    refactor and array

---
+++ src/main/java/org/qa23/arrays/Task10/Task10.java
@@ -0,0 +1,71 @@
+package org.qa23.arrays.Task10;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task10 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arrays = new int[50];
+        for (int i = 0; i < arrays.length; i++) {
+            arrays[i] = random.nextInt(-100, 100);
+        }
+
+        int countChet = 0;
+        int countNeChet = 0;
+        int countNegative = 0;
+        int countPositive = 0;
+        for (int i = 0; i < arrays.length; i++) {
+            if (arrays[i]> 0) {
+                countPositive++;
+            } else {
+                countNegative++;
+            }
+            if (arrays[i] %2 == 0) {
+                countChet++;
+            } else {
+                countNeChet++;
+            }
+        }
+        int[] chet = new int[countChet];
+        int[] neChet = new int[countNeChet];
+        int[] negative = new int[countNegative];
+        int[] positive = new int[countPositive];
+        for (int i = 0; i < arrays.length; i++) {
+            if (arrays[i] < 0) {
+                negative[(countNegative-1)] = arrays[i];
+                countNegative--;
+            } else {
+                positive[countPositive-1] = arrays[i];
+                countPositive--;
+            }
+            if (arrays[i] % 2 == 0) {
+                chet[countChet-1] = arrays[i];
+                countChet--;
+            } else {
+                neChet[countNeChet-1] = arrays[i];
+                countNeChet--;
+            }
+        }
+        System.out.print("Массив с четными элементами :");
+        for (int i = 0; i < chet.length; i++) {
+            System.out.print(chet[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Массив с нечетными элементами :");
+        for (int i = 0; i < neChet.length; i++) {
+            System.out.print(neChet[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Массив с положительными элементами :");
+        for (int i = 0; i < positive.length; i++) {
+            System.out.print(positive[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Массив с отрицательными элементами :");
+        for (int i = 0; i < negative.length; i++) {
+            System.out.print(negative[i] + " ");
+        }
+
+    }
+}
